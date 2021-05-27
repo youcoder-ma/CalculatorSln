81a4aeaa5ed28b70e1dd3f4150d9719f7dd0d142
youcoder-ma <rmaili.me@gmail.com>
2021-05-27 15:28:50 +0100

    Add project files.

---
+++ Calculator/MainActivity.cs
@@ -0,0 +1,107 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace Calculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private TextView calculatorText;
+        private string[] numbers = new string[2];
+        private string @operator;
+
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+        [Java.Interop.Export("ButtonClick")]
+        public void ButtonClick(View v)
+        {
+            Button button = (Button)v;
+            if ("0123456789.".Contains(button.Text))
+                AddDigitOrDecimalPoint(button.Text);
+            else if ("÷×+-".Contains(button.Text))
+                AddOperator(button.Text);
+            else if ("=" == button.Text)
+                Calculate();
+            else
+                Erase();
+        }
+
+        public void AddDigitOrDecimalPoint(string value)
+        {
+            int index = @operator == null ? 0 : 1;
+
+            if (value == "." && numbers[index].Contains("."))
+                return;
+            numbers[index] += value;
+
+            UpdateCalculatorText();
+        }
+
+        private void AddOperator(string value)
+        {
+            if (numbers[1] != null)
+            {
+                Calculate(value);
+                return;
+            }
+
+            @operator = value;
+
+            UpdateCalculatorText();
+        }
+
+        private void Calculate(string newOperator = null)
+        {
+            double? result = null;
+            double? first = numbers[0] == null ? null : (double?)double.Parse(numbers[0]);
+            double? second = numbers[1] == null ? null : (double?)double.Parse(numbers[1]);
+
+            switch (@operator)
+            {
+                case "÷":
+                    result = first / second;
+                    break;
+                case "×":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+            }
+
+            if (result != null)
+            {
+                numbers[0] = result.ToString();
+                @operator = newOperator;
+                numbers[1] = null;
+                UpdateCalculatorText();
+            }
+        }
+
+        private void Erase()
+        {
+            numbers[0] = numbers[1] = null;
+            @operator = null;
+            UpdateCalculatorText();
+        }
+
+        private void UpdateCalculatorText() => calculatorText.Text = $"{numbers[0]} {@operator} {numbers[1]}";
+
+    }
+}
